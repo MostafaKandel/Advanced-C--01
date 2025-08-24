b273c8f6d9f996879e5dcf15451f660bab5b4008
mostafa kandel <101183814+MostafaKandel@users.noreply.github.com>
2025-08-24 18:59:02 +0200

    Q5

--- Advanced-C#-01/Program.cs
+++ Advanced-C#-01/Program.cs
@@ -27,23 +27,48 @@ namespace Advanced_C__01
         //    }
         //}
 
-        public static List<int> EvenList(List<int> list) { 
-              
-            List<int> evenNumbers = new List<int>();
-            for (int i = 0; i < list.Count; i++)
+        //public static List<int> EvenList(List<int> list) { 
+
+        //    List<int> evenNumbers = new List<int>();
+        //    for (int i = 0; i < list.Count; i++)
+        //    {
+
+
+        //        if (list[i] > 0 && list[i] % 2 == 0)
+        //        {
+        //            evenNumbers.Add(list[i]);
+        //        }
+        //    }
+
+        //     return evenNumbers;
+
+        //}
+
+        #region Q5
+
+        public static int FirstNonRepeatedChar(string input)
+        {
+            Dictionary<char, int> repeated = new Dictionary<char, int>();
+            string stringInsensitive = input.ToLower();
+            // store char and count of repeating
+            foreach (char c in stringInsensitive)
             {
-
-
-                if (list[i] > 0 && list[i] % 2 == 0)
-                {
-                    evenNumbers.Add(list[i]);
-                }
+                if (repeated.ContainsKey(c))
+                    repeated[c]++;
+                else repeated[c] = 1;
             }
 
-             return evenNumbers;
-        
+            // find the first non-repeated char
+
+            for (int i = 0; i < stringInsensitive.Length; i++)
+            {
+                if (repeated[stringInsensitive[i]] == 1)
+                    return i;
+            }
+            return -1;
         }
-        
+
+        #endregion
         static void Main(string[] args)
         {
             #region Q1
@@ -84,17 +109,32 @@ namespace Advanced_C__01
 
             #region Q3
 
-            List<int> numbersList = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
-            Console.WriteLine("Before extract even");
-            foreach (int item in numbersList)
-                Console.WriteLine(item);
+            //List<int> numbersList = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
+            //Console.WriteLine("Before extract even");
+            //foreach (int item in numbersList)
+            //    Console.WriteLine(item);
 
-            Console.WriteLine("after extract");
-            
-            foreach (int item in EvenList(numbersList))
-                Console.WriteLine(item);
+            //Console.WriteLine("after extract");
+
+            //foreach (int item in EvenList(numbersList))
+            //    Console.WriteLine(item);
 
             #endregion
+
+            #region Q5
+
+            string s1 = "Hhello";
+            string s2 = "ahmed";
+
+            int first = FirstNonRepeatedChar(s1);
+            int second = FirstNonRepeatedChar(s2);
+
+            Console.WriteLine($"the string is : {s1} and the index of first non-repeated-char is {first} and the first non-repeated char is {s1[first]}");
+            Console.WriteLine($"the string is : {s2} and the index of first non-repeated-char is {second} and the first non-repeated char is {s2[second]}");
+
+            #endregion
+
+
         }
     }
 }
