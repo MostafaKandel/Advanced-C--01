dffdab4267fb8f7d9491047b7f123230e124233d
mostafa kandel <101183814+MostafaKandel@users.noreply.github.com>
2025-08-24 13:25:44 +0200

    Q1

---
+++ Advanced-C#-01/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Advanced_C__01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            // Var: is used for implicit typing. it tells the compiler to figure out the right type from the expression
+            var intRange = new Range<int>(10, 20);
+            Console.WriteLine(intRange.IsInRange(10)); 
+            Console.WriteLine(intRange.IsInRange(25));
+            Console.WriteLine("Length: " + intRange.Length());
+            #endregion
+        }
+    }
+}
---
+++ Advanced-C#-01/Range.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_C__01
+{
+    internal class Range<T> where T: IComparable<T> // i can here also <INumber> to make all methods in the class accept only numeric types
+
+    {
+        
+        #region properties 
+        public T Min { get; set; }
+        public T Max { get; set; }
+        #endregion
+        #region constracor
+        //●	Implement a constructor that takes the minimum and maximum values to define the range.
+        public Range(T min, T max)
+        {
+            if (min.CompareTo(max) > 0)
+                throw new ArgumentException(" the max value should be greater than min value");
+
+            Min = min;
+            Max = max;
+        }
+        #endregion
+
+        #region method
+        //Implement a method IsInRange(T value) that returns true if the given  value is within the range, otherwise false.
+        public bool IsInRange(T value)
+        {
+            return value.CompareTo(Min) >= 0 && value.CompareTo(Max) <= 0;
+        }
+
+
+        //Implement a method Length() that returns the length of the range(the difference between the maximum and minimum values).
+        /*
+                 public T Length()
+        {
+            return Max - Min;
+
+        }  // this in sont valid because T may be string anther type
+         */
+
+        // i can use constrains to be only use INumber<T> in the classs and in this case i will use normal operator
+
+        public double Length()// double: because it is work with all type of numeric
+        {
+            try
+            {
+                double minValue = Convert.ToDouble(Min);
+                double maxValue = Convert.ToDouble(Max);
+                return maxValue - minValue;
+            }
+            catch (Exception) { 
+                
+                throw new InvalidOperationException(" the Leength function can only work with numeric types");
+            }
+        } 
+        #endregion
+
+
+    }
+}
